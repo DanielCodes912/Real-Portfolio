033ad3e0f153b6b7a31f86513f8d0af6b1a1c388
TheRealDanielAjayi <120325196+DanielCodes912@users.noreply.github.com>
2025-03-15 20:00:30 +0100

    Update myPortfolio.js
    Modified js script plus got rid of comment.

--- myPortfolio.js
+++ myPortfolio.js
@@ -1,19 +1,16 @@
-// Get the button
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+  let myBtn = document.getElementById("myBtn");
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    myBtn.style.display = "flex";
   } else {
-    mybutton.style.display = "none";
+    myBtn.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  window.scrollTo({ top: 0, behavior: "smooth" });
 }
